871189563adb2b4f74232c65b4ad2d3fa952fcd9
Kairos22 <alandiko84@gmail.com>
2022-09-28 18:53:36 +0200

    Ex2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,55 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
 
-
+import dataImport from "./data.js";
+console.log(dataImport);
 // \/ All of your javascript should go here \/
+const dataParse = JSON.parse(dataImport);
+console.log(dataParse);
+const container = document.querySelector(".container");
 
+for (const [key, value] of Object.entries(recipesParsed)) {
+  createCard(value, key);
+}
+
+// Create Card Function
+function createCard(value, key) {
+  for (const item of value) {
+    // Card Container
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card");
+    cardContainer.style.width = "18rem";
+    cardContainer.style.marginRight = "2rem";
+
+    // Image
+    const img = document.createElement("img");
+    img.classList.add("card-img-top", "img-fluid");
+    img.src = item.image;
+    img.style.height = "14rem";
+
+    // Card Body
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    // Heading
+    const heading = document.createElement("h5");
+    heading.classList.add("card-title");
+    heading.textContent = item.title;
+
+    // Person Name
+    const personName = document.createElement("p");
+    personName.classList.add("card-text", "border-bottom");
+    personName.textContent = item.author;
+
+    // Ingredients
+    const ingredients = document.createElement("p");
+    ingredients.classList.add("card-text");
+    ingredients.textContent = item.ingredients;
+
+    cardContainer.append(img, cardBody);
+    cardBody.append(heading, personName, ingredients);
+    // body.append(cardContainer);
+    container.querySelector(`#${item.type}`).append(cardContainer);
+  }
+}
